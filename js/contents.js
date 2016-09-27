8d65fd94b0e3276dc5f19cbc6f2ff4f4d732ec26
Jack Loughran <loughranjack28@gmail.com>
2016-09-27 08:43:38 -0400

    changed mobile layout of post

--- js/contents.js
+++ js/contents.js
@@ -1,10 +1,12 @@
 $('document').ready(function () {
-  const contents = $('#contents');
+  const contentsDesktop = $('#contents-desktop');
+  const contentsMobile = $('#contents-mobile');
   $('h1').each(function (index, title) {
     if (index !== 0) {
       const titleText = $(this).text();
       const linkText = titleText.replace(' ', '-').toLowerCase();
-      contents.append('<li><a href="#' + linkText + '">' + titleText + '</a></li>');
+      contentsDesktop.append('<li><a href="#' + linkText + '">' + titleText + '</a></li>');
+      contentsMobile.append('<li><a href="#' + linkText + '">' + titleText + '</a></li>');
     }
   });
 
